ee9d4b89c1edf08a804b661154404248a2d17859
Alexandria Roberts <iamlexieonearth@gmail.com>
2023-12-13 09:50:12 +0800

    going through errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ export default function BlogPosts({ data }: { data: DataProps }) {
               </p>
             </div>
           </div>
-          <div className='mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
+          {/* <div className='mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
             {data.posts.map((post) => (
               <article
                 className='flex max-w-xl flex-col items-start justify-between'
@@ -54,10 +54,7 @@ export default function BlogPosts({ data }: { data: DataProps }) {
                 </div>
                 <div className='group relative'>
                   <h3 className='mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600'>
-                    <a href='#'>
-                      <span className='absolute inset-0'></span>
-                      {post.title}
-                    </a>
+                    <a href='#'>{post.title}</a>
                   </h3>
                   <p className='mt-5 line-clamp-3 text-sm leading-6 text-gray-600'>
                     {post.body}
@@ -71,17 +68,14 @@ export default function BlogPosts({ data }: { data: DataProps }) {
                   />
                   <div className='text-sm leading-6'>
                     <p className='font-semibold text-gray-900'>
-                      <a href='#'>
-                        <span className='absolute inset-0'></span>
-                        Michael Foster
-                      </a>
+                      <a href='#'>Michael Foster</a>
                     </p>
                     <p className='text-gray-600'>Co-Founder / CTO</p>
                   </div>
                 </div>
               </article>
             ))}
-          </div>
+          </div> */}
         </div>
       </main>
     </div>
